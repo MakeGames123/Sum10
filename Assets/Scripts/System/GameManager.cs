b0bf3a67f6185052705b17d51a66f2ebe51fc3e8
KJW <swjwpower@gmail.com>
2026-02-06 00:07:47 +0900

    bugfix

--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -153,8 +153,8 @@ public class GameManager : MonoBehaviour
         // 게임 시작 전 현재 주간 순위 + 전체 최고기록 저장 (완료까지 대기)
         if (scoreManager != null)
         {
-            await scoreManager.SavePreviousWeeklyRankAsync();
-            await scoreManager.SavePreviousHighScoreAsync();
+            scoreManager.SavePreviousWeeklyRankAsync();
+            scoreManager.SavePreviousHighScoreAsync();
         }
 
         progress = StartCoroutine(ProgressGame());
